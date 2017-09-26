6dac988c0c3c81e633afc764f3099c387b795478
Haozhou Wang <hawang@pivotal.io>
2017-09-26 15:00:09 +0800

    Fix R_HOME path issue
    1. Remove R_HOME export in script to avoid conflict
    2. Add R_HOME check when starting client

--- bin/rclient.sh
+++ bin/rclient.sh
@@ -4,13 +4,6 @@
 # Copyright (c) 2016-Present Pivotal Software, Inc
 #
 # ------------------------------------------------------------------------------
-
-if [ -d /usr/local/greenplum-db ] ; then
-    source /usr/local/greenplum-db/greenplum_path.sh
-else
-    export R_HOME=/usr/lib64/R
-fi
-
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/clientdir
 
 cd /clientdir
