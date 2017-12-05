50dd2bb19b423bd54aeff11458f6ac886de0dfcf
Haozhou Wang <hawang@pivotal.io>
2017-12-05 10:25:05 +0800

    Reformat the source code
    Use tab to instead of space

--- rlogging.h
+++ rlogging.h
@@ -9,12 +9,18 @@
 
 #include <R.h>
 
-SEXP plr_debug( SEXP args );
-SEXP plr_log( SEXP args );
-SEXP plr_info( SEXP args );
-SEXP plr_notice( SEXP args );
-SEXP plr_warning( SEXP args );
-SEXP plr_error( SEXP args );
-SEXP plr_fatal( SEXP args );
+SEXP plr_debug(SEXP args);
+
+SEXP plr_log(SEXP args);
+
+SEXP plr_info(SEXP args);
+
+SEXP plr_notice(SEXP args);
+
+SEXP plr_warning(SEXP args);
+
+SEXP plr_error(SEXP args);
+
+SEXP plr_fatal(SEXP args);
 
 #endif /* PLC_RLOGGING_H */
